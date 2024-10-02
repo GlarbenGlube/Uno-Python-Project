b892aa361c3efc9e1f3dc208c36006b0adbadf04
Glarb <134835936+GlarbenGlube@users.noreply.github.com>
2024-10-02 11:21:31 +0200

    Add files via upload

---
+++ UNO.py
@@ -0,0 +1,67 @@
+
+
+'''
+
+
+
+
+Brainstorm:
+
+    Rules:
+        - alle spillere får 7 kort, kortene er tilfældig
+        - der er 3 spillere (1 menneske, 2 computere)
+        - det skal ikke være muligt at trække når du har lyst, kun når du ikke har nogle kort at spille
+        - hvis du trækker et kort der kan spilles så må du lægge kortet, hvis ikke, så bliver din tur sprunget over
+        - kun 1 kort må lægges af gangen, selv hvis du har 2 identiske kort, så er det kun 1 kort der må lægges
+        - h
+    
+
+    GUI:
+        - lav en gui som et grafisk interface der viser kortene
+        - Kortene skal illustreres ved hjælp af billeder der tages fra en mappe på pc'en
+
+    
+    
+    Special cards and adjustments:
+
+        Adjustments:
+            +4 og wildcards:
+            i stedet for at farven kan vælges, så kommer +4 og wildcards med en bestemt farve
+            kortet kan stadig bruges på hvilket som helst tidspunkt, men er ikke lige så versitil som før
+
+
+        Special cards:    
+            shuffle: 
+            tager alle kort, blander dem og uddeler dem så alle spillere får lige mange kort
+            fx. 
+            player 1 = 10 kort  
+            player 2 = 10 kort 
+            player 3 = 1 kort
+            total = 21
+            *shuffle kort bruges* 
+            total / spiller = 21/3 = 7, så alle ender med 7 kort, uanset om de havde 
+            flere eller færre kort før shuffle blev brugt
+
+            
+            random:
+            tager et tilfældig kort fra en anden spillers hånd og erstatter det med et andet tilfældig kort
+
+            
+            swap:
+            bytter din hånd med en anden spillers hånd, du vælger sælg hvem du bytter med
+
+            
+            rotate:
+            rotere alle spillers hånd med uret, så 
+            player 1's hånd går til player 2
+            player 2's hånd går til player 3
+            player 3's hånd går til player 1
+
+
+            Bomb:
+            hvis bombe kortet spilles så bliver der startet en timer, hvis et kort ikke er blevet spillet
+            før timeren løber ud, så springer bomben og du trækker kort
+
+
+
+'''
